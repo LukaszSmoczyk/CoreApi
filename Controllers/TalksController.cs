e68a106b3d3ff79289f44da9fc50833a2e477cd2
LukaszSmoczyk <lukaszsmoczyk@gmail.com>
2021-01-08 23:17:53 +0100

    post a new talk

--- Controllers/TalksController.cs
+++ Controllers/TalksController.cs
@@ -31,7 +31,7 @@ namespace CoreCodeCamp.Controllers
         {
             try
             {
-                var talks = await _repository.GetTalksByMonikerAsync(moniker);
+                var talks = await _repository.GetTalksByMonikerAsync(moniker, true);
                 return _mapper.Map<TalkModel[]>(talks);
             }
             catch (Exception)
@@ -45,13 +45,49 @@ namespace CoreCodeCamp.Controllers
         {
             try
             {
-                var talk = await _repository.GetTalkByMonikerAsync(moniker, id);
+                var talk = await _repository.GetTalkByMonikerAsync(moniker, id, true);
                 return _mapper.Map<TalkModel>(talk);
             }
             catch (Exception)
             {
 
                 return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get Talk");
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<TalkModel>> Post(string moniker, TalkModel model)
+        {
+            try
+            {
+                var camp = await _repository.GetCampAsync(moniker);
+                if (camp == null) return BadRequest("Camp does not exist");
+
+                var talk = _mapper.Map<Talk>(model);
+                talk.Camp = camp;
+
+                if (model.Speaker == null) return BadRequest("Speaker ID is required");
+                var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                if (speaker == null) return BadRequest("Couldn't find speaker");
+                talk.Speaker = speaker;
+
+                _repository.Add(talk);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    var url = _linkGenerator.GetPathByAction(HttpContext,
+                        "Get",
+                        values: new { moniker, id = talk.TalkId });
+
+                    return Created(url, _mapper.Map<TalkModel>(talk));
+                } 
+                else
+                {
+                    return BadRequest("failed to save new Talk");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to post Talk");
             }
         }
 
